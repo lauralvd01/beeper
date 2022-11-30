cb31073533c398dd1037d5711570024b76b9e991
lauralvd01 <laura.levraud1@etu.univ-lorraine.fr>
2022-11-30 09:40:05 +0100

    beginning2

---
+++ api/src/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+ res.status(200).send("Hello world");
+});
+
+app.listen(8080);

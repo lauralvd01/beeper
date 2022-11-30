108d730850cc65754c8539d8ef32007fe2aed173
lauralvd01 <laura.levraud1@etu.univ-lorraine.fr>
2022-11-30 10:25:46 +0100

    auth

--- api/src/index.js
+++ api/src/index.js
@@ -1,14 +1,17 @@
+import "dotenv/config.js"
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
+import { checkJwt } from "./auth/jwt-middleware.js";
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(checkJwt);
 
 app.get("/", (req, res) => {
- res.status(200).send("Hello world");
+ res.status(200).send(`Hello ${req.auth.sub}`);
 });
 
 app.listen(8080);
